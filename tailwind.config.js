aecd8f255f3960e082ac059cf37878699f195bf1
neveca <117100709+neveca@users.noreply.github.com>
2024-01-03 23:42:44 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      },
+      colors:{
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#4caf4f", 
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+      },
+      /*Ketik  repeating radial gradient di pencarian*/
+      backgroundImage: {
+        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px);',
+        circularDark: 'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #1b1b1b 8px, #1b1b1b 100px);'
+      }
+    },
+  },
+  plugins: [],
+}
+
